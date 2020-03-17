c886360cce3991e817e0685a5ea96ca37a32eb95
Tyler Morales <moratyle@gmail.com>
2020-03-17 12:32:20 -0500

    render home page

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const heroBtn = document.querySelector('.hero--btn')
+const arrowEl = document.querySelector('.arrow')
+const btnTextEl = document.querySelector('.btn--text')
+
+heroBtn.addEventListener('mouseenter', (e) => {
+    arrowEl.classList.add('arrow-hover')
+    btnTextEl.classList.add('btn-text-hover')
+})
+heroBtn.addEventListener('mouseleave', (e) => {
+    arrowEl.classList.remove('arrow-hover')
+    btnTextEl.classList.remove('btn-text-hover')
+
+})
